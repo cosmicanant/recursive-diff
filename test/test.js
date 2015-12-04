6ad218790a151e937507fde9a956121c690b92e1
Isabella Oliveira <isabella.oliveira@zeedhi.com>
2015-12-04 10:45:38 -0200

    Implementing visitorCallback and improving tests

--- test/test.js
+++ test/test.js
@@ -1,67 +1,75 @@
 var diff = require('../index.js');
-var assert = require('assert') ;
+var assert = require('assert');
 
-var a, b, delta, c ;
+var a, b, delta, c;
 
-//testing primitive data type
-a = 3 ;
-b = 10;
-delta = diff.getDiff(a, b); 
-c = diff.applyDiff(a, delta);
-assert.equal(b,c);
+describe("diff tests", function() {
 
-//testing array
-a = [1,2,3,4] ;
-b = [2,3,4] ;
-delta = diff.getDiff(a, b); 
-c = diff.applyDiff(a, delta);
-assert.deepEqual(b,c);
+    it("testing primitive data type", function() {
+        a = 3;
+        b = 10;
+        delta = diff.getDiff(a, b);
+        c = diff.applyDiff(a, delta);
+        assert.equal(b, c);
+    });
 
-//testing object 
-a = {
-    a: '10',
-    b: '20'
-} ;
-b = {
-    a: '10',
-    b: '40'
-} ;
-delta = diff.getDiff(a, b); 
-c = diff.applyDiff(a, delta);
-assert.deepEqual(b,c);
+    it("testing array", function() {
+        a = [1, 2, 3, 4];
+        b = [2, 3, 4];
+        delta = diff.getDiff(a, b);
+        //console.log(delta);
+        c = diff.applyDiff(a, delta);
+        assert.deepEqual(b, c);
+    });
 
-//testing complex deep object
-var fn = function(){}; //two function can be equal if they hold same reference
-a = {
-    a: fn,
-    b: [1,2,[3,4]],
-    c: {
-        c1 : 20,
-        c2 : {
-            c21: 'hello'
-        },
-        c3: 'India'
-    } 
-} ;
-b = {
-    a: fn,
-    b: [1,2,[4]],
-    c: {
-        c1 : 20,
-        c2 : {
-            c21: 'hi',
-            c22: 'welcome'
-        },
-        c3: 'cosmic' 
-    } 
-} ;
+    it("testing object ", function() {
+        a = {
+            a: '10',
+            b: '20'
+        };
+        b = {
+            a: '10',
+            b: '40'
+        };
+        delta = diff.getDiff(a, b);
+        c = diff.applyDiff(a, delta);
+        assert.deepEqual(b, c);
+    });
 
-console.log('Object1 is:')
-console.log(a);
-console.log('Object2 is:')
-console.log(b);
-delta = diff.getDiff(a, b);
-console.log('Diff calculated is:')
-console.log(delta);
-c = diff.applyDiff(a, delta);
-assert.deepEqual(b,c);
+    it("testing complex deep object", function() {
+        var fn = function() {}; //two function can be equal if they hold same reference
+        a = {
+            a: fn,
+            b: [1, 2, [3, 4]],
+            c: {
+                c1: 20,
+                c2: {
+                    c21: 'hello'
+                },
+                c3: 'India'
+            }
+        };
+        b = {
+            a: fn,
+            b: [1, 2, [4]],
+            c: {
+                c1: 20,
+                c2: {
+                    c21: 'hi',
+                    c22: 'welcome'
+                },
+                c3: 'cosmic'
+            }
+        };
+
+        //console.log('Object1 is:')
+        //console.log(a);
+        //console.log('Object2 is:')
+        //console.log(b);
+        delta = diff.getDiff(a, b);
+        //console.log('Diff calculated is:')
+        //console.log(delta);
+        c = diff.applyDiff(a, delta);
+        assert.deepEqual(b, c);
+    });
+});
