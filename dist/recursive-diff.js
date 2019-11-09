02cd4129fd8e407fb34efd20b2d402fe7b03e9c3
Anant Shukla <anant.shukla.rkgit@gmail.com>
2019-11-09 14:25:49 +0530

    seperate build for dev and prod env

--- dist/recursive-diff.js
+++ dist/recursive-diff.js
@@ -1,1 +1,111 @@
-!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports.recursiveDiff=t():e.recursiveDiff=t()}("undefined"!=typeof self?self:this,(function(){return function(e){var t={};function o(n){if(t[n])return t[n].exports;var r=t[n]={i:n,l:!1,exports:{}};return e[n].call(r.exports,r,r.exports,o),r.l=!0,r.exports}return o.m=e,o.c=t,o.d=function(e,t,n){o.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},o.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},o.t=function(e,t){if(1&t&&(e=o(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(o.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var r in e)o.d(n,r,function(t){return e[t]}.bind(null,r));return n},o.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return o.d(t,"a",t),t},o.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},o.p="",o(o.s=0)}([function(e,t){function o(e){let t=typeof e;return"object"===t&&(e instanceof Array?t="array":e instanceof Date&&(t="date")),t}function n(e,t,o,n){if(!t.match(/^\//))throw new Error(`Diff path: "${t}" is not valid`);const r=t.split("/");r.shift();let i=e;const{length:l}=r;for(let f=0;f<l;f+=1){if(!i)throw new Error(`Invalid path: "${t}" for object: ${JSON.stringify(e,null,2)}`);if(r[f].length<1)throw new Error(`Invalid path: "${t}" for object: ${JSON.stringify(e,null,2)}`);f!==l-1?(i=i[r[f]],n&&n(i)):i[r[f]]=o}return e}function r(e,t){const n=t.split("/");n.shift();let r=e;const{length:i}=n;for(let l=0;l<i;l+=1)if(l!==i-1){if(!r[n[l]])throw new Error(`Invalid path: "${t}" for object: ${JSON.stringify(e,null,2)}`);r=r[n[l]]}else if("object"===o(r))delete r[n[l]];else{const e=parseInt(n[l],10);for(;r.length>e;)r.pop()}return e}e.exports={getDiff:function e(t,n,r,i){let l,f,u,a;const p=o(t),d=o(n);if(null!=r&&"string"==typeof r||(r="/"),null!=i&&"object"==typeof i||(i={}),null==t||null==n)t!==n&&(i[r]="undefined"===p?{operation:"add",value:n}:"undefined"===d?{operation:"delete"}:{operation:"update",value:n});else if(p!==d||"object"!==p&&"array"!==p)t!==n&&(p===d&&"date"===p?t.getTime()!==n.getTime()&&(i[r]={operation:"update",value:n}):i[r]={operation:"update",value:n});else{let p=Object.keys(t);for(let d=0;d<p.length;d+=1)a=p[d],u="/"===r?r+a:`${r}/${a}`,l=t[a],f=n[a],null==l||null==f?l!==f&&(i[u]=void 0===l?{operation:"add",value:f}:void 0===f?{operation:"delete"}:{operation:"update",value:f}):o(l)!==o(f)?i[u]={operation:"update",value:f}:"object"==typeof l?e(l,f,u,i):l!==f&&(i[u]={operation:"update",value:f});p=Object.keys(n);for(let e=0;e<p.length;e+=1)a=p[e],u="/"===r?r+a:`${r}/${a}`,(l=t[a])!==(f=n[a])&&void 0===l&&(i[u]={operation:"add",value:f})}return i},applyDiff:function(e,t,o){let i,l,f;if(!t)throw new Error("No diff object is provided, Nothing to apply");const u=Object.keys(t);for(let a=0;a<u.length;a+=1){const p=u[a];if(i=p,!(f=(l=t[p]).operation).match(/add|update|delete/))throw new Error(`Invalid operation: "${f}"`);if("add"===f){if("/"===i){e=l.value;break}n(e,i,l.value,o)}else if("update"===f){if("/"===i){e=l.value;break}n(e,i,l.value,o)}else{if("/"===i){e=null;break}r(e,i)}}return e}}}])}));
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["recursiveDiff"] = factory();
+	else
+		root["recursiveDiff"] = factory();
+})((typeof self !== 'undefined' ? self : this), function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/recursive-diff.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/recursive-diff.js":
+/*!*******************************!*\
+  !*** ./src/recursive-diff.js ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("/* eslint-disable no-param-reassign */\nfunction getType(x) {\n  let type = typeof x;\n  if (type === 'object') {\n    if (x instanceof Array) {\n      type = 'array';\n    } else if (x instanceof Date) {\n      type = 'date';\n    }\n  }\n  return type;\n}\n\nfunction getDiff(ob1, ob2, path, result) {\n  let val1; let val2; let newpath; let key;\n  const type1 = getType(ob1);\n  const type2 = getType(ob2);\n  // initialize some defaults\n  if (path == null || typeof path !== 'string') {\n    path = '/'; // initialize to root path\n  }\n  if (result == null || typeof result !== 'object') {\n    result = {};\n  }\n  // diff algo\n  if (ob1 == null || ob2 == null) {\n    if (ob1 !== ob2) {\n      if (type1 === 'undefined') {\n        result[path] = { operation: 'add', value: ob2 };\n      } else if (type2 === 'undefined') {\n        result[path] = { operation: 'delete' };\n      } else {\n        result[path] = { operation: 'update', value: ob2 };\n      }\n    }\n  } else if (type1 !== type2 || (type1 !== 'object' && type1 !== 'array')) {\n    if (ob1 !== ob2) {\n      if (type1 === type2 && type1 === 'date') {\n        if (ob1.getTime() !== ob2.getTime()) {\n          result[path] = { operation: 'update', value: ob2 };\n        }\n      } else {\n        result[path] = { operation: 'update', value: ob2 };\n      }\n    }\n  } else {\n    let keys = Object.keys(ob1);\n    for (let i = 0; i < keys.length; i += 1) {\n      key = keys[i];\n      newpath = path === '/' ? path + key : `${path}/${key}`;\n      val1 = ob1[key];\n      val2 = ob2[key];\n\n      if (val1 == null || val2 == null) {\n        if (val1 !== val2) {\n          if (typeof val1 === 'undefined') {\n            result[newpath] = { operation: 'add', value: val2 };\n          } else if (typeof val2 === 'undefined') {\n            result[newpath] = { operation: 'delete' };\n          } else {\n            result[newpath] = { operation: 'update', value: val2 };\n          }\n        }\n      } else if (getType(val1) !== getType(val2)) {\n        result[newpath] = { operation: 'update', value: val2 };\n      } else if (typeof val1 === 'object') {\n        getDiff(val1, val2, newpath, result);\n      } else if (val1 !== val2) {\n        result[newpath] = { operation: 'update', value: val2 };\n      }\n    }\n    keys = Object.keys(ob2);\n    for (let i = 0; i < keys.length; i += 1) {\n      key = keys[i];\n      newpath = path === '/' ? path + key : `${path}/${key}`;\n      val1 = ob1[key];\n      val2 = ob2[key];\n      if (val1 !== val2) {\n        if (typeof val1 === 'undefined') {\n          result[newpath] = { operation: 'add', value: val2 };\n        }\n      }\n    }\n  }\n  return result;\n}\n\n\nfunction setValueByPath(ob, path, value, visitorCallback) {\n  if (!path.match(/^\\//)) {\n    throw new Error(`Diff path: \"${path}\" is not valid`);\n  }\n  const keys = path.split('/');\n  keys.shift();\n  let val = ob;\n  const { length } = keys;\n  for (let i = 0; i < length; i += 1) {\n    if (!val) throw new Error(`Invalid path: \"${path}\" for object: ${JSON.stringify(ob, null, 2)}`);\n    else if (keys[i].length < 1) throw new Error(`Invalid path: \"${path}\" for object: ${JSON.stringify(ob, null, 2)}`);\n\n    if (i !== length - 1) {\n      val = val[keys[i]];\n      if (visitorCallback) {\n        visitorCallback(val);\n      }\n    } else {\n      val[keys[i]] = value;\n    }\n  }\n  return ob;\n}\n\nfunction deleteValueByPath(ob, path) {\n  const keys = path.split('/');\n  keys.shift(); // removing initial blank element ''\n  let val = ob;\n  const { length } = keys;\n  for (let i = 0; i < length; i += 1) {\n    if (i !== length - 1) {\n      if (!val[keys[i]]) {\n        throw new Error(`Invalid path: \"${path}\" for object: ${JSON.stringify(ob, null, 2)}`);\n      }\n      val = val[keys[i]];\n    } else if (getType(val) === 'object') {\n      delete val[keys[i]];\n    } else {\n      const index = parseInt(keys[i], 10);\n      while (val.length > index) {\n        val.pop();\n      }\n    }\n  }\n  return ob;\n}\n\nfunction applyDiff(ob1, diff, visitorCallback) {\n  let path; let diffOb; let\n    op;\n  if (!diff) throw new Error('No diff object is provided, Nothing to apply');\n  const keys = Object.keys(diff);\n  for (let i = 0; i < keys.length; i += 1) {\n    const key = keys[i];\n    path = key;\n    diffOb = diff[key];\n    op = diffOb.operation;\n    if (op.match(/add|update|delete/)) {\n      if (op === 'add') {\n        if (path === '/') {\n          ob1 = diffOb.value;\n          break;\n        }\n        setValueByPath(ob1, path, diffOb.value, visitorCallback);\n      } else if (op === 'update') {\n        if (path === '/') {\n          ob1 = diffOb.value;\n          break;\n        }\n        setValueByPath(ob1, path, diffOb.value, visitorCallback);\n      } else {\n        if (path === '/') {\n          ob1 = null;\n          break;\n        }\n        deleteValueByPath(ob1, path);\n      }\n    } else throw new Error(`Invalid operation: \"${op}\"`);\n  }\n  return ob1;\n}\n\nmodule.exports = {\n  getDiff,\n  applyDiff,\n};\n\n\n//# sourceURL=webpack://recursiveDiff/./src/recursive-diff.js?");
+
+/***/ })
+
+/******/ });
+});
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 module.exports = {
   entry: './src/recursive-diff.js',
-  mode: 'production',
+  mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'recursive-diff.min.js',
+    filename: 'recursive-diff.js',
     library: 'recursiveDiff',
     libraryTarget: 'umd',
     globalObject: '(typeof self !== \'undefined\' ? self : this)',
